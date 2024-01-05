ad70e12a4c0e51a212961dbc7ef5bf0ef3abeda6
Rodrigo Andrade <rodrigormda@hotmail.com>
2024-01-04 22:42:01 -0300

    Scripted auto-commit on change (2024-01-04 22:42:01) by gitwatch.sh

--- maintenance_tracker/reset_db.py
+++ maintenance_tracker/reset_db.py
@@ -5,13 +5,13 @@ import requests
 if __name__ == "__main__":
 
     print("OI")
-    arg = sys.argv[1].lower()
-    if arg == "init_km":
-        initial_km = float(sys.argv[2])
-    else:
-        initial_km = 0
-    url = f"http://192.168.100.96:7125/server/database/item?namespace=maintenance_tracker&key=init_value&value={initial_km * 1000 * 1000}`"
-    requests.post(url)
-    url = f"http://192.168.100.96:7125/server/database/item?namespace=maintenance_tracker&key=curr_value&value={initial_km * 1000 * 1000}`"
-    requests.post(url)
-    print(f"Database reset to: {initial_km} km")
+    # arg = sys.argv[1].lower()
+    # if arg == "init_km":
+    #     initial_km = float(sys.argv[2])
+    # else:
+    #     initial_km = 0
+    # url = f"http://192.168.100.96:7125/server/database/item?namespace=maintenance_tracker&key=init_value&value={initial_km * 1000 * 1000}`"
+    # requests.post(url)
+    # url = f"http://192.168.100.96:7125/server/database/item?namespace=maintenance_tracker&key=curr_value&value={initial_km * 1000 * 1000}`"
+    # requests.post(url)
+    # print(f"Database reset to: {initial_km} km")
