b11b4ec927a0df7fce12f3b1538f9dbeb2d5ad32
Rodrigo Andrade <rodrigormda@hotmail.com>
2024-01-04 23:05:54 -0300

    Scripted auto-commit on change (2024-01-04 23:05:54) by gitwatch.sh

--- maintenance_tracker/reset_db.py
+++ maintenance_tracker/reset_db.py
@@ -18,22 +18,22 @@ def reset_db(inital_km):
 def query_db():
     url = f"http://192.168.100.96:7125/server/database/item?namespace=maintenance_tracker&key=init_value"
     ret = requests.get(url)
-    init_value = ret.json()["result"]["value"]
+    init_value = float(ret.json()["result"]["value"])
     init_value = init_value / 1000 / 1000
 
     url = f"http://192.168.100.96:7125/server/database/item?namespace=maintenance_tracker&key=curr_value_x"
     ret = requests.get(url)
-    curr_value_x = ret.json()["result"]["value"]
+    curr_value_x = float(ret.json()["result"]["value"])
     curr_value_x = (curr_value_x - init_value) / 1000 / 1000
 
     url = f"http://192.168.100.96:7125/server/database/item?namespace=maintenance_tracker&key=curr_value_y"
     ret = requests.get(url)
-    curr_value_y = ret.json()["result"]["value"]
+    curr_value_y = float(ret.json()["result"]["value"])
     curr_value_y = (curr_value_y - init_value) / 1000 / 1000
 
     url = f"http://192.168.100.96:7125/server/database/item?namespace=maintenance_tracker&key=curr_value_z"
     ret = requests.get(url)
-    curr_value_z = ret.json()["result"]["value"]
+    curr_value_z = float(ret.json()["result"]["value"])
     curr_value_z = (curr_value_z - init_value) / 1000 / 1000
 
     print(f"Health of X axis: {curr_value_x / init_value:.2%}% (your X axis has traveled {curr_value_x:.2f} km)")
