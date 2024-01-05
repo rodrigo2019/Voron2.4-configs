456e6a623a3ef2d75fa2e594c33543c33125d4c4
Rodrigo Andrade <rodrigormda@hotmail.com>
2024-01-05 14:41:01 -0300

    Scripted auto-commit on change (2024-01-05 14:41:01) by gitwatch.sh

--- maintenance_tracker/maintenance_tracker.py
+++ maintenance_tracker/maintenance_tracker.py
@@ -10,65 +10,44 @@ def _read_gcode(filename):
     with open(filename) as f:
         lines = f.readlines()
 
-    lines = [line for line in lines if
-             line.startswith("G90") or line.startswith("G91") or line.startswith("G1") or line.startswith("G0")]
+    valid_commands = {"G90", "G91", "G1", "G0"}
+    lines = [line for line in lines if line.split()[0] in valid_commands]
 
-    # sum all distance from x, y and z
     mode = "absolute"
-    x = 0
-    y = 0
-    z = 0
-    last_x = 0
-    last_y = 0
-    last_z = 0
+    distances = {"x": 0, "y": 0, "z": 0}
+    last_positions = {"x": 0, "y": 0, "z": 0}
+
     for line in lines:
-        if line.startswith("G90"):
+        command, *values = line.split()
+
+        if command == "G90":
             mode = "absolute"
-        elif line.startswith("G91"):
+        elif command == "G91":
             mode = "relative"
-        elif line.startswith("G1") or line.startswith("G0"):
-            if "X" in line:
-                if mode == "absolute":
-                    current_x = float(line.split("X")[1].split(" ")[0])
-                    x += abs(current_x - last_x)
-                    last_x = current_x
-                else:
-                    x += float(line.split("X")[1].split(" ")[0])
-            if "Y" in line:
-                if mode == "absolute":
-                    current_y = float(line.split("Y")[1].split(" ")[0])
-                    y += abs(current_y - last_y)
-                    last_y = current_y
-                else:
-                    y += float(line.split("Y")[1].split(" ")[0])
-            if "Z" in line:
-                if mode == "absolute":
-                    current_z = float(line.split("Z")[1].split(" ")[0])
-                    z += abs(current_z - last_z)
-                    last_z = current_z
-                else:
-                    z += float(line.split("Z")[1].split(" ")[0])
-    return x, y, z
+        elif command in ["G1", "G0"]:
+            for axis in ["X", "Y", "Z"]:
+                if axis in values:
+                    current_value = float(values[values.index(axis) + 1])
+                    distances[axis.lower()] += abs(
+                        current_value - last_positions[axis.lower()]) if mode == "absolute" else current_value
+                    last_positions[axis.lower()] = current_value
+
+    return distances["x"], distances["y"], distances["z"]
 
 
 def _update_odometer(x=None, y=None, z=None):
-    url = f"{MOONRAKER_URL}/server/database/item?namespace=maintenance_tracker"
+    base_url = f"{MOONRAKER_URL}/server/database/item?namespace=maintenance_tracker"
 
-    if x is not None:
-        ret = requests.get(url + "&key=curr_value_x")
-        curr_value_x = float(ret.json()["result"]["value"])
-        x += curr_value_x
-        requests.post(url + f"&key=curr_value_x&value={x}")
-    if y is not None:
-        ret = requests.get(url + "&key=curr_value_y")
-        curr_value_y = float(ret.json()["result"]["value"])
-        y += curr_value_y
-        requests.post(url + f"&key=curr_value_y&value={y}")
-    if z is not None:
-        ret = requests.get(url + "&key=curr_value_z")
-        curr_value_z = float(ret.json()["result"]["value"])
-        z += curr_value_z
-        requests.post(url + f"&key=curr_value_z&value={z}")
+    def update_axis(axis, value):
+        if value is not None:
+            ret = requests.get(f"{base_url}&key=curr_value_{axis}")
+            curr_value = float(ret.json()["result"]["value"])
+            new_value = value + curr_value
+            requests.post(f"{base_url}&key=curr_value_{axis}&value={new_value}")
+
+    update_axis('x', x)
+    update_axis('y', y)
+    update_axis('z', z)
 
 
 def process_gcode(filename):
@@ -77,15 +56,16 @@ def process_gcode(filename):
     query_db()
 
 
-def process_history():
+def process_history(gcode_folder):
     # iterate over folder for all *.gcode files
-    files = os.listdir("/home/rodrigo/printer_data/gcodes")
+
+    files = os.listdir(gcode_folder)
     files = [file for file in files if file.endswith(".gcode")]
     total_x = 0
     total_y = 0
     total_z = 0
     for file in files:
-        file = f"/home/rodrigo/printer_data/gcodes/{file}"
+        file = f"{gcode_folder}/{file}"
         x, y, z = _read_gcode(file)
         total_x += x
         total_y += y
@@ -96,7 +76,7 @@ def process_history():
 
 
 def reset_db(initial_km):
-    base_url = "http://127.0.0.1:7125/server/database/item?namespace=maintenance_tracker"
+    base_url = f"{MOONRAKER_URL}/server/database/item?namespace=maintenance_tracker"
 
     # Reset init_value
     init_value_url = f"{base_url}&key=init_value&value={initial_km * 1000 * 1000}"
@@ -111,32 +91,26 @@ def reset_db(initial_km):
 
 
 def query_db():
-    url = f"http://127.0.0.1:7125/server/database/item?namespace=maintenance_tracker&key=init_value"
-    ret = requests.get(url)
-    init_value = float(ret.json()["result"]["value"])
-    init_value = init_value / 1000 / 1000
+    base_url = f"{MOONRAKER_URL}/server/database/item?namespace=maintenance_tracker"
 
-    url = f"http://127.0.0.1:7125/server/database/item?namespace=maintenance_tracker&key=curr_value_x"
-    ret = requests.get(url)
-    curr_value_x = float(ret.json()["result"]["value"])
-    curr_value_x = curr_value_x / 1000 / 1000
+    def get_and_convert_value(key):
+        url = f"{base_url}&key={key}"
+        ret = requests.get(url)
+        value = float(ret.json()["result"]["value"])
+        return value / 1000 / 1000
 
-    url = f"http://127.0.0.1:7125/server/database/item?namespace=maintenance_tracker&key=curr_value_y"
-    ret = requests.get(url)
-    curr_value_y = float(ret.json()["result"]["value"])
-    curr_value_y = curr_value_y / 1000 / 1000
+    init_value = get_and_convert_value("init_value")
+    curr_value_x = get_and_convert_value("curr_value_x")
+    curr_value_y = get_and_convert_value("curr_value_y")
+    curr_value_z = get_and_convert_value("curr_value_z")
 
-    url = f"http://127.0.0.1:7125/server/database/item?namespace=maintenance_tracker&key=curr_value_z"
-    ret = requests.get(url)
-    curr_value_z = float(ret.json()["result"]["value"])
-    curr_value_z = curr_value_z / 1000 / 1000
+    health_x = (init_value - curr_value_x) / init_value
+    health_y = (init_value - curr_value_y) / init_value
+    health_z = (init_value - curr_value_z) / init_value
 
-    print(
-        f"Health of X axis: {(init_value - curr_value_x) / init_value:.2%}% (your X axis has traveled {curr_value_x:.3f} km)")
-    print(
-        f"Health of Y axis: {(init_value - curr_value_y) / init_value:.2%}% (your Y axis has traveled {curr_value_y:.3f} km)")
-    print(
-        f"Health of Z axis: {(init_value - curr_value_z) / init_value:.2%}% (your Z axis has traveled {curr_value_z:.3f} km)")
+    print(f"Health of X axis: {health_x:.2%} (your X axis has traveled {curr_value_x:.3f} km)")
+    print(f"Health of Y axis: {health_y:.2%} (your Y axis has traveled {curr_value_y:.3f} km)")
+    print(f"Health of Z axis: {health_z:.2%} (your Z axis has traveled {curr_value_z:.3f} km)")
 
 
 if __name__ == "__main__":
@@ -147,8 +121,12 @@ if __name__ == "__main__":
         reset_db(initial_km_)
     elif arg == "query":
         query_db()
-    elif arg == "process_history":
-        process_history()
-    elif arg == "process_gcode":
-        filename_ = f"/home/rodrigo/printer_data/gcodes/{sys.argv[2]}"
-        process_gcode(filename_)
+    elif arg == "process_history" or arg == "process_gcode":
+        ret = requests.get(f"{MOONRAKER_URL}/server/files/roots")
+        folders = ret.json()["result"]
+        gcode_folder_ = [folder for folder in folders if folder["name"] == "gcodes"][0]["path"]
+        if arg == "process_history":
+            process_history(gcode_folder_)
+        else:
+            filename_ = f"{gcode_folder_}/{sys.argv[2]}"
+            process_gcode(filename_)
